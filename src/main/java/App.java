ec247a5e4b29971e675e21b7bab01d4b6fbc4b9e
Charawey-X <charawey@gmail.com>
2022-05-12 21:42:08 +0300

    added layout and home page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,20 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        port(4200);
+
+        //home page
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "welcome.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
