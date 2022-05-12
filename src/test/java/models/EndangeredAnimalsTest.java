adca0419077d3ac30eb437d9a6b07867e0688515
Charawey-X <charawey@gmail.com>
2022-05-12 19:35:24 +0300

    endangered animal test

--- src/test/java/models/EndangeredAnimalsTest.java
+++ src/test/java/models/EndangeredAnimalsTest.java
@@ -1,7 +1,63 @@
 package models;
 
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EndangeredAnimalsTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
 
+    private EndangeredAnimals setUpNewAnimal() {
+        return new EndangeredAnimals("White Rhino","endangered","healthy","young");
+    }
+    @Test
+    public void testInstanceOfEndangeredAnimalsClass_true(){
+        EndangeredAnimals testAnimal= setUpNewAnimal();
+        assertTrue(testAnimal instanceof EndangeredAnimals);
+    }
+    @Test
+    public void allInstancesAreSaved(){
+        EndangeredAnimals testAnimal=setUpNewAnimal();
+        testAnimal.save();
+        assertTrue(EndangeredAnimals.all().get(0).getHealth().equals(testAnimal.getHealth()));
+    }
+    @Test
+    public void findByIdReturnsCorrectInfo(){
+        EndangeredAnimals testAnimal=setUpNewAnimal();
+        testAnimal.save();
+        Animals foundAnimal= Animals.find(testAnimal.getId());
+        assertEquals(foundAnimal.getHealth(), testAnimal.getHealth());
+
+    }
+    @Test
+    public void deleteByID(){
+        EndangeredAnimals testAnimal=setUpNewAnimal();
+        testAnimal.save();
+        testAnimal.delete();
+        assertNull(Animals.find(testAnimal.getId()));
+
+    }
+    @Test
+    public void deleteAllEntries(){
+        EndangeredAnimals testAnimal=setUpNewAnimal();
+        EndangeredAnimals otherAnimal=setUpNewAnimal();
+        testAnimal.save();
+        otherAnimal.save();
+        Animals.deleteAll();
+        List<Animals> animals=Animals.all();
+        assertEquals(0,animals.size());
+    }
+    @Test
+    public void ensureNameFieldCannotBeEmpty(){
+        EndangeredAnimals testAnimal=new EndangeredAnimals("","endangered","","");
+        try {
+            testAnimal.save();
+        }catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
