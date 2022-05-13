0130dea8725f2f2ff68ed56b782da21c292837ea
Charawey-X <charawey@gmail.com>
2022-05-13 14:28:24 +0300

    Generated pom.xml file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,9 +9,16 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[] args) {
         staticFileLocation("/public");
-        port(4200);
+        port(getHerokuAssignedPort());
         //welcome page
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
